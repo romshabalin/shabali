d50463d34383c918f39fbf76006a02015c8c1891
Roman Shabalin <romshabalin@gmail.com>
2025-12-11 21:32:29 +0500

    Update header.php

--- include/html/header.php
+++ include/html/header.php
@@ -4,4 +4,15 @@
 </head>
 <body>
 <header>
+    <div class="container-fluid px-0">
+        <div class="rom g-0">
+            <div class="col-12 p-3">
+                <p class="mb-0">
+                    <a href="/" class="text-dark text-decoration-none">
+                        shabali
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
 </header>
