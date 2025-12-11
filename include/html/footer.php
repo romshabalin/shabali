c5c59b3b4d29c1406e21223d9176bde7baa3b19b
Roman Shabalin <romshabalin@gmail.com>
2025-12-11 21:42:29 +0500

    Update footer.php

--- include/html/footer.php
+++ include/html/footer.php
@@ -3,6 +3,7 @@
         <div class="rom g-0">
             <div class="col-12 p-3">
                 <p class="mb-0">
+                    2025
                 </p>
             </div>
         </div>
