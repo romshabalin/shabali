938c4ac5f045ae513872eebeacd2aee8fad5675b
Roman Shabalin <romshabalin@gmail.com>
2025-12-11 18:31:09 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<main>
+  <div class="container px-0">
+    <div class="row g-0">
+      <div class="col-12 p-3">
+        <h1 class="mb-0">
+          <?php echo $h1; ?>
+        </h1>
+      </div>
+    </div>
+  </div>
+</main>
