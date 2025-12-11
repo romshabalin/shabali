b5ab8ea6dfa635f9c021705204cf6d9b28ffdbba
Roman Shabalin <romshabalin@gmail.com>
2025-12-11 21:24:04 +0500

    Update footer.php

--- include/html/footer.php
+++ include/html/footer.php
@@ -1,2 +1,10 @@
 <footer>
+    <div class="container-fluid px-0">
+        <div class="rom g-0">
+            <div class="col-12 p-3">
+                <p class="mb-0">
+                </p>
+            </div>
+        </div>
+    </div>
 </footer>
