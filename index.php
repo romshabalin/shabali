17aa94a0cec029ae9de4d532d4596658666093d2
Roman Shabalin <romshabalin@gmail.com>
2025-12-13 14:37:10 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $title = "";
 $description = "";
 $canonical = "/";
+$h1 = "";
+$p = "";
 
 required_once "./include/html/header.php";
 
@@ -12,12 +14,12 @@ required_once "./include/html/header.php";
         <div class="row g-0">
             <div class="col-12 p-3 text-center">
                 <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
-                    <?php echo $title; ?>
+                    <?php echo $h1; ?>
                 </h1>
             </div>
             <div class="col-12 p-3 text-center">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    <?php echo $description; ?>
+                    <?php echo $p; ?>
                 </p>
             </div>
         </div>
