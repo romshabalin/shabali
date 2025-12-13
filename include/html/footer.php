bba6e28c2827567407ed5802a9b3edfb3bc6c4bb
Roman Shabalin <romshabalin@gmail.com>
2025-12-13 19:58:02 +0500

    Update footer.php

--- include/html/footer.php
+++ include/html/footer.php
@@ -2,6 +2,12 @@
     <div class="container-fluid px-0">
         <div class="rom g-0">
             <div class="col-12 p-3">
+            </div>
+            <div class="col-12 p-3">
+            </div>
+            <div class="col-12 p-3">
+            </div>
+            <div class="col-12 p-3">
                 <p class="mb-0 fs-6 fw-normal lh-sm text-dark">
                     &#169;&#160;2025&#160;shabali
                 </p>
