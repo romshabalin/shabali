477d11238ccd0c9d7d6ae5f6aa24aa9a72b4e423
Roman Shabalin <romshabalin@gmail.com>
2025-12-13 13:00:47 +0500

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ required_once "./include/html/header.php";
     <div class="container px-0">
         <div class="row g-0">
             <div class="col-12 p-3 text-center">
-                <h1 class="mb-0">
+                <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
                     <?php echo $title; ?>
                 </h1>
             </div>
             <div class="col-12 p-3 text-center">
-                <p class="mb-0">
+                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     <?php echo $description; ?>
                 </p>
             </div>
