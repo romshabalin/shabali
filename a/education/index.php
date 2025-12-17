773246e279d6bee0542a2494c10c89fec80fa867
Roman Shabalin <romshabalin@gmail.com>
2025-12-17 19:45:27 +0500

    Create index.php

---
+++ a/education/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$title = "";
+$description = "";
+$canonical = "/a/education/";
+$h1 = "";
+$p = "";
+
+require_once './../../include/html/header.php';
+
+?>
+<main>
+    <div class="container px-0">
+        <div class="row g-0">
+            <div class="col-12 p-3 text-center">
+                <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
+                    <?php echo $h1; ?>
+                </h1>
+            </div>
+            <div class="col-12 p-3 text-center">
+                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
+                    <?php echo $p; ?>
+                </p>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+
+require_once './../../include/html/footer.php';
+
+?>
