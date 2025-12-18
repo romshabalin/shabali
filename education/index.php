13b476c856756ea1fc4bc5b6aaa95f64d73de890
Roman Shabalin <romshabalin@gmail.com>
2025-12-18 18:21:49 +0500

    Update index.php

--- education/index.php
+++ education/index.php
@@ -1,1 +1,53 @@
+<?php
 
+$title = "Образование";
+$description = "";
+$canonical = "/education/";
+$h1 = "Образование";
+$p = "";
+
+require_once './../include/html/header.php';
+
+?>
+<main>
+    <div class="container px-0">
+        <div class="row g-0">
+            <div class="col-12 p-3 text-center">
+                <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
+                    <?php echo $h1; ?>
+                </h1>
+            </div>
+            <div class="col-12 p-3 text-center">
+                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
+                    <?php echo $p; ?>
+                </p>
+            </div>
+            <div class="">
+                <h2 class="">
+                </h2>
+            </div>
+            <div class="">
+                <h3 class="">
+                </h3>
+            </div>
+            <div class="">
+                <h3 class="">
+                </h3>
+            </div>
+            <div class="">
+                <h3 class="">
+                </h3>
+            </div>
+            <div class="">
+                <h2 class="">
+                    См. также
+                </h2>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+
+require_once './../include/html/footer.php';
+
+?>
