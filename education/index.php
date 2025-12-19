98169c5ec68649f7d040d8456846a08e51302034
Roman Shabalin <romshabalin@gmail.com>
2025-12-19 18:28:35 +0500

    Update index.php

--- education/index.php
+++ education/index.php
@@ -22,61 +22,6 @@ require_once './../include/html/header.php';
                     <?php echo $p; ?>
                 </p>
             </div>
-            <div class="col-12 p-3">
-                <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
-                    Виды образования
-                </h2>
-            </div>
-            <div class="col-12 p-3">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Общее образование
-                </h3>
-            </div>
-            <div class="col-12 p-3">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Среднее образование
-                </h3>
-            </div>
-            <div class="col-12 p-3">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Высшее образование
-                </h3>
-            </div>
-            <div class="col-12 p-3">
-                <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
-                    См. также
-                </h2>
-            </div>
-            <div class="col-12 p-3">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark"><!-- Образование -->
-                    Бумажное образование
-                </h3>
-            </div>
-            <div class="col-12 p-3">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Фундаментальное бумажное образование (Fundamental Paper Education, сокращённо FPE) - анимационный веб-сериал на YouTube, созданный автором Kaaatie.
-                </p>
-            </div>
-            <div class="col-12 p-3">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
-                    Фанфики - Бумажное образование (FPE - фанфики)
-                </h4>
-            </div>
-            <div class="col-12 p-3">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Фанфики по вселенной - Фундаментальное бумажное образование, можно найти на нескольких платформах.
-                </p>
-            </div>
-            <div class="col-12 p-3">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Фанфик по Бумажному образованию мисс Циркуль и Эбии
-                </h5>
-            </div>
-            <div class="col-12 p-3">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Фанфик - Неожиданное открытие.
-                </p>
-            </div>
         </div>
     </div>
 </main>
