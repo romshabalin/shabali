a9ab6cbe34e91f7d31bf4184c0ead4cc6ffd4d79
Roman Shabalin <romshabalin@gmail.com>
2025-12-20 09:56:02 +0500

    Update index.php

--- education/index.php
+++ education/index.php
@@ -12,27 +12,27 @@ require_once './../include/html/header.php';
 <main>
     <div class="container px-0">
         <div class="row g-0">
-            <div class="col-12 p-3 text-center">
+            <div class="col-12 p-3 pb-2 text-center">
                 <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
                     <?php echo $h1; ?>
                 </h1>
             </div>
-            <div class="col-12 p-3 text-center">
+            <div class="col-12 p-3 pt-2 text-center">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     <?php echo $p; ?>
                 </p>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pb-2">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Общее образование
                 </h2>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pt-2">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Включает четыре уровня.
                 </p>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 ">
                 <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Дошкольное образование
                 </h3>
@@ -52,32 +52,32 @@ require_once './../include/html/header.php';
                     Среднее общее образование
                 </h3>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pb-2">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Профессиональное образование
                 </h2>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pt-2">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Включает два уровня.
                 </p>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pb-2">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Дополнительное образование
                 </h2>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pt-2">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Включает два вида.
                 </p>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pb-2">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Профессиональное обучение
                 </h2>
             </div>
-            <div class="col-12 p-3">
+            <div class="col-12 p-3 pt-2">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Обучение технике пожарной безопасности и т. п.
                 </p>
