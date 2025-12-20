b744942441b4e044df392d094a5591c76adc0aa6
Roman Shabalin <romshabalin@gmail.com>
2025-12-20 18:11:04 +0500

    Update index.php

--- index.php
+++ index.php
@@ -12,34 +12,34 @@ require_once './include/html/header.php';
 <main>
     <div class="container px-0">
         <div class="row g-0">
-            <div class="col-12 p-3 pb-2 text-center">
+            <div class="col-12 p-3 pb-1 text-center">
                 <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
                     <?php echo $h1; ?>
                 </h1>
             </div>
-            <div class="col-12 p-3 pt-2 text-center">
+            <div class="col-12 p-3 pt-1 text-center">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     <?php echo $p; ?>
                 </p>
             </div>
-            <div class="col-12 p-3 pb-2">
+            <div class="col-12 p-3 pb-1">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     <a href="/education/" class="text-dark text-decoration-none">
                         Образование
                     </a>
                 </h2>
             </div>
-            <div class="col-12 p-3 pt-2">
+            <div class="col-12 p-3 pt-1">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Фундамент для самореализации.
                 </p>
             </div>
-            <div class="col-12 p-3 pb-2">
+            <div class="col-12 p-3 pb-1">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Карьера
                 </h2>
             </div>
-            <div class="col-12 p-3 pt-2">
+            <div class="col-12 p-3 pt1">
                 <p class="mb-0 fs-6 fw-normal lh-base text-dark">
                     Самореализация.
                 </p>
