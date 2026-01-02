6eac21ecc0c2e5c8815995e78fa5ac6d282cf030
Roman Shabalin <romshabalin@gmail.com>
2026-01-02 13:12:04 +0500

    Update index.php

--- education/index.php
+++ education/index.php
@@ -22,206 +22,90 @@ require_once './../include/html/header.php';
                     <?php echo $p; ?>
                 </p>
             </div>
-            <div class="col-12 p-3 pb-1">
+            <div class="col-12 p-3">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
-                    Виды образования в России
-                </h2>
-            </div>
-            <div class="col-12 p-3 pt-1 pb-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    В соответствии с Федеральным законом от 29.12.2012 №&#8201;273-ФЗ &#171;Об образовании в Российской Федерации&#187;, система образования в России подразделяется на четыре вида.
-                </p>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    <a href="https://www.consultant.ru/document/cons_doc_LAW_140174/" class="text-dark text-decoration-none">
-                        www.consultant.ru
-                    </a>
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Общее образование
-                </h3>
+                </h2>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Включает четыре уровня.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Дошкольное образование
-                </h4>
+                </h3>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Детский сад или центр развития.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Начальное общее образование
-                </h4>
+                </h3>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    1&#8211;4 класс школы, лицея или гимназии.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Основное общее образование
-                </h4>
+                </h3>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    5&#8211;9 класс школы, лицея или гимназии.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
                     Среднее общее образование
-                </h4>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    10&#8211;11 класс школы, лицея или гимназии.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Профессиональное образование
                 </h3>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Включает два уровня.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
-                    Среднее профессиональное образование (СПО)
-                </h4>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Колледж или техникум.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
-                    Высшее образование
-                </h4>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Институт, академия или университет. Включает три уровня.
-                </p>
-            </div>
-            <div class="col-12 p-3">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Базовое высшее образования (БВО)
-                </h5>
-            </div>
-            <div class="col-12 p-3">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Специализированное высшее образования (СПВО)
-                </h5>
-            </div>
-            <div class="col-12 p-3">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Аспирантура
-                </h5>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Дополнительное образование
-                </h3>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Включает два вида.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
-                    Дополнительное образование детей и взрослых
-                </h4>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Творческие кружки, спортивные секции, языковые курсы и т. п.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
-                    Дополнительное профессиональное образование (ДПО)
-                </h4>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Только для имеющих или получающих среднее профессиональное образование (СПО) или высшее образование. Включает два вида.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Программа повышения квалификации
-                </h5>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Углубление знаний в своей профессии.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h5 class="mb-0 fs-5 fw-medium lh-sm text-dark">
-                    Программа профессиональной переподготовки
-                </h5>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Смена квалификации.
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
-                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Профессиональное обучение
-                </h3>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    Водитель, маляр, косметолог, мастер маникюра и педикюра, повар, официант, архивариус, страховой агент, рекламный агент, санитар, обучение технике пожарной безопасности и т. п.
-                </p>
             </div>
             <div class="col-12 p-3">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
-                    См. также
+                    Профессиональное образование
                 </h2>
             </div>
-            <div class="col-12 p-3 pb-1">
+            <div class="col-12 p-3">
                 <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    Минобрнауки России (Министерство науки и высшего образования Российской Федерации)
+                    Среднее профессиональное образование (СПО)
                 </h3>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    <a href="https://minobrnauki.gov.ru/" class="text-dark text-decoration-none">
-                        minobrnauki.gov.ru
-                    </a>
-                </p>
-            </div>
-            <div class="col-12 p-3 pb-1">
+            <div class="col-12 p-3">
                 <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
-                    КонсультантПлюс
+                    Высшее образование
                 </h3>
             </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    <a href="https://www.consultant.ru/" class="text-dark text-decoration-none">
-                        www.consultant.ru
-                    </a>
-                </p>
+            <div class="col-12 p-3">
+                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+                    Базовое высшее образования (БВО)
+                </h4>
+            </div>
+            <div class="col-12 p-3">
+                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+                    Специализированное высшее образования (СПВО)
+                </h4>
+            </div>
+            <div class="col-12 p-3">
+                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+                    Аспирантура
+                </h4>
+            </div>
+            <div class="col-12 p-3">
+                <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
+                    Дополнительное образование
+                </h2>
+            </div>
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
+                    Дополнительное образование детей и взрослых
+                </h3>
+            </div>
+            <div class="col-12 p-3">
+                <h3 class="mb-0 fs-3 fw-medium lh-sm text-dark">
+                    Дополнительное профессиональное образование (ДПО)
+                </h3>
+            </div>
+            <div class="col-12 p-3">
+                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+                    Программа повышения квалификации
+                </h4>
+            </div>
+            <div class="col-12 p-3">
+                <h4 class="mb-0 fs-4 fw-medium lh-sm text-dark">
+                    Программа профессиональной переподготовки
+                </h4>
+            </div>
+            <div class="col-12 p-3">
+                <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
+                    Профессиональное обучение
+                </h2>
             </div>
         </div>
     </div>
