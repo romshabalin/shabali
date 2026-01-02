474911d798c844f4691b013393c62012ab395880
Roman Shabalin <romshabalin@gmail.com>
2026-01-02 22:21:22 +0500

    Update index.php

--- hobby/index.php
+++ hobby/index.php
@@ -9,27 +9,11 @@ $p = "Отдых.";
 require_once './../include/html/header.php';
 
 ?>
-<main>
-    <div class="container px-0">
-        <div class="row g-0">
-            <div class="col-12 p-3 pb-1">
-                <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
-                    <?php echo $h1; ?>
-                </h1>
-            </div>
-            <div class="col-12 p-3 pt-1">
-                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
-                    <?php echo $p; ?>
-                </p>
-            </div>
             <div class="col-12 p-3">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Путешествия
                 </h2>
             </div>
-        </div>
-    </div>
-</main>
 <?php
 
 require_once './../include/html/footer.php';
