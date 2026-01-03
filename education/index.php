5754813062508367bb0ee40787a1308b2f252afe
Roman Shabalin <romshabalin@gmail.com>
2026-01-03 16:22:24 +0500

    Update index.php

--- education/index.php
+++ education/index.php
@@ -9,6 +9,19 @@ $p = "Фундамент для самореализации.";
 require_once './../include/html/header.php';
 
 ?>
+<main>
+    <div class="container px-0">
+        <div class="row g-0">
+            <div class="col-12 p-3 pb-1">
+                <h1 class="mb-0 fs-1 fw-medium lh-sm text-dark">
+                    <?php echo $h1; ?>
+                </h1>
+            </div>
+            <div class="col-12 p-3 pt-1">
+                <p class="mb-0 fs-6 fw-normal lh-base text-dark">
+                    <?php echo $p; ?>
+                </p>
+            </div>
             <div class="col-12 p-3">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Общее образование
@@ -69,6 +82,9 @@ require_once './../include/html/header.php';
                     Профессиональное обучение
                 </h2>
             </div>
+        </div>
+    </div>
+</main>
 <?php
 
 require_once './../include/html/footer.php';
