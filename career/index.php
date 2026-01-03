d7faef876d363bb385a0c438ae66f6adade6957c
Roman Shabalin <romshabalin@gmail.com>
2026-01-03 16:26:02 +0500

    Update index.php

--- career/index.php
+++ career/index.php
@@ -24,11 +24,6 @@ require_once './../include/html/header.php';
             </div>
             <div class="col-12 p-3">
                 <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
-                    Медицина
-                </h2>
-            </div>
-            <div class="col-12 p-3">
-                <h2 class="mb-0 fs-2 fw-medium lh-sm text-dark">
                     Информационные технологии (ИТ)
                 </h2>
             </div>
